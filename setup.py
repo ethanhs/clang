1a44d495ba3e180bae214d1fa48e09672e5ed682
Ethan Smith <ethan@ethanhs.me>
2018-06-10 20:13:30 -0700

    Add README as long description

--- setup.py
+++ setup.py
@@ -1,4 +1,11 @@
 from setuptools import setup
+from os import path
+
+# from https://packaging.python.org/guides/making-a-pypi-friendly-readme/
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
 
 setup(
     name="clang",
@@ -7,4 +14,6 @@ setup(
     version="6.0.0",
     url="https://github.com/ethanhs/clang",
     packages=["clang"],
+    long_description=long_description,
+    long_description_content_type='text/markdown'
 )
