28e3989d399d24fdbd994d630cbcde297e426c71
Ethan Smith <ethan@ethanhs.me>
2018-08-02 20:16:20 -0700

    Fix install for Python 2

--- setup.py
+++ setup.py
@@ -1,6 +1,9 @@
 from setuptools import setup
 from os import path
+import sys
 
+if sys.version_info < (3, 0, 0):
+    from io import open  # python 2 builtin open doesn't have an encoding option
 # from https://packaging.python.org/guides/making-a-pypi-friendly-readme/
 this_directory = path.abspath(path.dirname(__file__))
 with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
